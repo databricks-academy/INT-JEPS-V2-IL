3f2607544af0ce28a78a846bde76ba7ea2f11b9e
SireInsectus <>
2020-09-21 18:42:59 +0000

    Reset to match the currently published version

---
+++ Notebooks/Version-2.0.1.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC 
+// MAGIC %md # Course: JEPFS
+// MAGIC * Version 2.0.1
+// MAGIC * Built 2020-09-02 17:47:19 UTC
+// MAGIC * Git revision: 7fc8922baa758c34ae57c82dd255e3dfaa434076
+// MAGIC 
+// MAGIC Copyright © 2020 Databricks, Inc.
