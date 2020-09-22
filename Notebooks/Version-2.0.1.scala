f37d368e5bf9976d4f3969418824945a53beff3c
SireInsectus <>
2020-09-22 00:59:02 +0000

    Renamed notebook to follow current standards

--- Notebooks/Version-2.0.1.scala
+++
@@ -1,7 +0,0 @@
-// Databricks notebook source
-// MAGIC %md # Course: JEPFS
-// MAGIC * Version 2.0.1
-// MAGIC * Built 2020-09-02 17:47:19 UTC
-// MAGIC * Git revision: 7fc8922baa758c34ae57c82dd255e3dfaa434076
-// MAGIC 
-// MAGIC Copyright © 2020 Databricks, Inc.
